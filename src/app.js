755fd7fa40d2a345baff55a9cfdd494e8d5e08fb
ismaelRivera96 <144409241+ismaelRivera96@users.noreply.github.com>
2025-05-09 02:38:05 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from 'express'; 
-import marketRoutes from './routes/market.routes.js' 
-const app=express() 
+import marketRoutes from './routes/market.routes.js'; 
+
+const app = express(); 
+
 // Middleware para procesar JSON 
 app.use(express.json()); 
-// Middleware para procesar datos de formularios (opcional, si usas x-www-formurlencoded) 
+
+// Middleware para procesar datos de formularios (opcional)
 app.use(express.urlencoded({ extended: true })); 
-app.use(marketRoutes) 
+
+// Rutas
+app.use(marketRoutes); 
+
+// Middleware para rutas no encontradas
 app.use((req, res, next) => { 
-res.status(404).json({ 
-message: "Favor realizar pruebas en los siguientes endpoints:", 
-endpoints: [ 
-"https://apimarket-production-28fc.up.railway.app/usuarios", 
-"https://apimarket-production-28fc.up.railway.app/productos" 
-] 
+  res.status(404).json({ 
+    message: "Favor realizar pruebas en los siguientes endpoints:", 
+    endpoints: [ 
+      "https://apimarket-production-28fc.up.railway.app/usuarios", 
+      "https://apimarket-production-28fc.up.railway.app/productos" 
+    ] 
+  }); 
 }); 
-}); 
+
 export default app;
